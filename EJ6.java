46f9a4ef445bfab5d50ab5ed39aff4580d6d47b6
juancaballero5 <jcabcas2806@g.educaand.es>
2025-11-06 06:06:17 +0100

    Add files via upload

---
+++ EJ6.java
@@ -0,0 +1,34 @@
+public class EJ6 {
+    public static void main(String[] args) {
+        int i = 5;
+        int numero = (int) (Math.random() * (100 - 0 + 1)) + 0;
+        int intento;
+
+        System.out.println("Estoy pensando un numero del 0 al 100. Tienes 5 intentos para adivinarlo.");
+        System.out.print("Introduce un numero: ");
+        intento = Integer.parseInt(System.console().readLine());
+
+        while ((intento != numero) || (i < 0)) {
+            if (intento < 0 || intento > 100) {
+                System.out.println("**Error numero introducido no valido");
+
+                if (intento < numero) {
+                    System.out.printf("El numero en el que estoy pensando es mayor que %d \n", intento);
+                } else if (intento > numero) {
+                    System.out.printf("El numero en el que estoy pensando es menor que %d \n", intento);
+                }
+
+                System.out.printf("Te quedan %d intentos.\n", i);
+                System.out.print("Introduce un numero: ");
+                intento = Integer.parseInt(System.console().readLine());
+                i--;
+            }
+        }
+        if (i == 0) {
+            System.out.printf("Lo siento, no has adivinado el numero. Era el %d\n", numero);
+        } else {
+            System.out.printf("Enhorabuena! Has adivinado el numero %d\n", numero);
+        }
+
+    }
+}
---
+++ Ej7.java
@@ -0,0 +1,54 @@
+public class Ej7 {
+    public static void main(String[] args) {
+        int i = 0;
+        int cantidad = 1;
+        int local;
+        int visitante;
+
+        try {
+            for (i = 0; i < 14; i++) {
+                int numero1 = (int) (Math.random() * (2 - 0 + 1)) + 0;
+                int numero2 = (int) (Math.random() * (2 - 0 + 1)) + 0;
+                int numero3 = (int) (Math.random() * (2 - 0 + 1)) + 0;
+
+                if (numero1 == 0) {
+                    numero1 = 'X';
+                } else if (numero2 == 0) {
+                    numero2 = 'X';
+                } else if (numero3 == 0) {
+                    numero3 = 'X';
+                }
+
+                System.out.printf("%d: |%d | %d | %d |\n", cantidad, numero1, numero2, numero3);
+                cantidad++;
+
+            }
+
+            System.out.print(" Pleno al 15 - ");
+            System.out.print("Local ...");
+            local = Integer.parseInt(System.console().readLine());
+            if (local < 0) {
+                System.out.println("**Error numero introducido no valido");
+            }
+            if (local >= 0 && local <= 2) {
+                System.out.printf("%d |", local);
+            } else if (local > 2) {
+                System.out.print("M |");
+            }
+
+            System.out.print(" Visitante ...");
+            visitante = Integer.parseInt(System.console().readLine());
+            if (visitante < 0) {
+                System.out.println("**Error numero introducido no valido");
+            }
+            if (visitante >= 0 && visitante <= 2) {
+                System.out.printf("%d \n", visitante);
+            } else if (visitante > 2) {
+                System.out.println("M");
+            }
+
+        } catch (Exception e) {
+            System.out.println("**Error");
+        }
+    }
+}
