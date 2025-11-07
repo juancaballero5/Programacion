8e62af15defee25508c677587569e98f0843e4f9
juancaballero5 <jcabcas2806@g.educaand.es>
2025-11-07 08:23:36 +0100

    Add BucleFor class to print numbers 1 to 10

---
+++ Programacion/Tema05/BucleFor.java
@@ -0,0 +1,11 @@
+public class BucleFor {
+    public static void main(String[] args) {
+        //PARA contador = 1 HASTA 10 PASO 1 HACER ... FINPARA
+
+        int i;
+
+        for (i = 1; i <=10;i++) {
+            System.err.println(i);
+        }
+}
+}
