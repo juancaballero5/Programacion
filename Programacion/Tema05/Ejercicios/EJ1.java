a5715345e1201fd2c499df1735be59b9faea6c57
juancaballero5 <jcabcas2806@g.educaand.es>
2025-11-20 12:04:43 +0100

    Create EJ1.java

---
+++ Programacion/Tema05/Ejercicios/EJ1.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class EJ1 {
+    public static void main(String[] args) {
+        
+        int i;
+
+        for (i = 0 ; i <= 100;i += 5) {
+            System.out.print(i + " " );
+        }
+    }
+}
