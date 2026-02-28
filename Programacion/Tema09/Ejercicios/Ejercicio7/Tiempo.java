59a921c39bd4bd1e91d63809ceee5dde4717591e
juancaballero5 <jcabcas2806@g.educaand.es>
2026-02-28 17:32:04 +0100

    caca

---
+++ Programacion/Tema09/Ejercicios/Ejercicio7/Tiempo.java
@@ -0,0 +1,27 @@
+package Ejercicios.Ejercicio7;
+
+public class Tiempo {
+    
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Tiempo (int hora,int minuto,int segundo) {
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    public void suma() {
+        
+    }
+
+    public void resta() {
+
+    }
+
+    @Override
+    public String toString() { 
+        return String.format("pizza %dh %dm %ds");
+    }
+}
