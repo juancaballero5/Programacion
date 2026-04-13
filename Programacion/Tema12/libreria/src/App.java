96651290894cb53c78a10034f054bc918df00328
Juan Caballero Castillo <jcabcas2806@g.educaand.es>
2026-04-13 11:16:51 +0200

    caca

--- Programacion/Tema12/libreria/src/App.java
+++ Programacion/Tema12/libreria/src/App.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String url = "jdbc:mysql://localhost:3306/ejemplo?characterEncoding=UTF=8";
+
+        try {
+            
+            Connection conexion = DriverManager.getConnection(url,"root", "root");
+
+            System.out.println("**INFO: Se ha conectado correctamente con la base de datos.");
+
+        } catch (SQLException e) {
+            System.out.println("**ERROR, Ha sido imposible conectarse a la base de datos");
+        }
     }
 }
