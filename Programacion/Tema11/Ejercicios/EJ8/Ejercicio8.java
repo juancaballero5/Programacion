0ec6865091fff4aefe86cc0b05c7e72914976f1e
juancaballero5 <jcabcas2806@g.educaand.es>
2026-04-14 16:54:44 +0200

    caca

--- Programacion/Tema11/Ejercicios/EJ8/Ejercicio8.java
+++ Programacion/Tema11/Ejercicios/EJ8/Ejercicio8.java
@@ -2,9 +2,12 @@ package Ejercicios.EJ8;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
@@ -22,15 +25,26 @@ public class Ejercicio8 {
             String linea = "";
             ArrayList<String> palabras = new ArrayList<>();
 
-            while (linea != null) {
-                linea = br.readLine();
-                palabras.add(linea);
+            while ((linea = br.readLine()) != null) {
+                if (!linea.trim().isEmpty()) {       
+                    palabras.add(linea.trim());
+                }
             }
 
+            Collections.sort(palabras, String.CASE_INSENSITIVE_ORDER);
+
+            for (String palabra : palabras) {
+                bw.write(palabra);
+                bw.newLine();
+            }
+
+            br.close();
+            bw.close();
             
-            
-        } catch (Exception e) {
-            
+        } catch (FileNotFoundException  e) {
+            System.out.println("**ERROR, el fichero no ha sido encontrado.");
+        } catch (IOException e) {
+            System.out.println("**ERROR, el fichero no ha podido leerse correctamente.");
         }
     }
 }
