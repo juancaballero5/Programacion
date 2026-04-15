dc6c453ab7002a57ac1ba0a375e6fc65ad6ecec7
juancaballero5 <jcabcas2806@g.educaand.es>
2026-04-15 19:26:21 +0200

    caca

--- Programacion/Tema11/Ejercicios/EJ15/Acronimo.java
+++ Programacion/Tema11/Ejercicios/EJ15/Acronimo.java
@@ -4,5 +4,27 @@ public class Acronimo {
     
     public static void main(String[] args) {
         
+        String argumento;
+        String acronimo = "";
+
+        if (args.length > 0) {
+            
+            for (int i = 0; i < args.length; i++) {
+                
+                argumento = args[i];
+        
+                if ( argumento.length() > 3) {
+                    
+                    acronimo += argumento.toUpperCase().charAt(0);
+    
+                } 
+            }
+    
+            System.out.println("Acrónimo: " + acronimo);
+        
+        } else {
+            System.out.println("ERROR: debes introducir al menos una palabra.");
+        }
+
     }
 }
