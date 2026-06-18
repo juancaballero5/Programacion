f478c2dda74dd3de92ec65d451ef2984a24fc473
juancaballero5 <jcabcas2806@g.educaand.es>
2026-06-18 08:57:12 +0200

    caca

--- examen_prog/evilrank/Prueba.java
+++
@@ -1,9 +0,0 @@
-public class Prueba {
-    
-    public static void main(String[] args) {
-        
-        Villano villano1 = new Villano("Antonio", "calvo", "Suspender", 100);
-
-        System.out.println(villano1);
-    }
-}
